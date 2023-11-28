4aacbff07c2792a5331d53bf7bc90ecef9db4e42
sarahthebest <sarah.emmoth@proton.me>
2023-11-28 09:40:11 +0100

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  "default-src*; style-src 'self' *;"
+  "default-src*; style-src 'self' *;";
   next();
 });
 
@@ -23,6 +23,29 @@ app.get("/", function (req, res) {
 });
 
 app.get("/guestforum", function (req, res) {
+  con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test",
+  });
+  con.connect(function (err) {
+    if (err) throw err;
+    con.query("SELECT * FROM messages", function (err, result, fields) {
+      if (err) throw err;
+      fs.readFile("guestforum.html", "utf-8", function (err, data) {
+        console.log("--------------" + data);
+        for (let message of result) {
+          output += "<tr>";
+          for (let key in message) {
+            output += `<td>${message[key]}</td>`;
+          }
+          output += "</tr>";
+        }
+        // console.log(result[0].name);
+      });
+    });
+  });
   res.sendFile(path.join(__dirname, "/public/guestforum.html"));
 });
 
@@ -31,23 +54,45 @@ app.get("/startpage", function (req, res) {
 });
 
 app.post("/guestforum", function (req, res) {
-  let messagesFile = fs.readFileSync("messages.json");
-  let data = JSON.parse(messagesFile);
-  let messages = data["messages"];
+  con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test",
+  });
 
-  const submission = {
-    name: req.body.name,
-    email: req.body.email,
-    title: req.body.title,
-    message: req.body.message,
-  };
-
-  messages.push(submission);
-  data["messages"] = messages;
-
-  const submissionJSON = JSON.stringify(data);
-  fs.writeFileSync("messages.json", submissionJSON);
-
-  res.redirect("/guestforum");
-
+  con.connect(function (err) {
+    const currentDate = new Date().toDateString();
+    // skriv till databasen
+    console.log("Uppkopplad till databas!");
+    let sql = `INSERT INTO messages (name, email, title, message, date)
+    VALUES ('${req.body.name}', '${req.body.email}', '${req.body.title}', '${req.body.message}', '${currentDate}')`;
+    console.log(sql);
+    con.query(sql, function (err, result) {
+      if (err) console.log(err);
+      res.redirect("/guestforum"); // gå tillbaka till get-route
+    });
+  });
 });
+
+// app.post("/guestforum", function (req, res) {
+//   let messagesFile = fs.readFileSync("messages.json");
+//   let data = JSON.parse(messagesFile);
+//   let messages = data["messages"];
+
+//   const submission = {
+//     name: req.body.name,
+//     email: req.body.email,
+//     title: req.body.title,
+//     message: req.body.message,
+//   };
+
+//   messages.push(submission);
+//   data["messages"] = messages;
+
+//   const submissionJSON = JSON.stringify(data);
+//   fs.writeFileSync("messages.json", submissionJSON);
+
+//   res.redirect("/guestforum");
+
+// });
